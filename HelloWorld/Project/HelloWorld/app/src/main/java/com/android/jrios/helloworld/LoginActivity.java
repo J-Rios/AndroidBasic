e39ecfae66c88dd96029ee67e636cecf87df4abb
J-Rios <jrios.rubio23@gmail.com>
2016-04-05 17:05:22 +0200

    HelloWorld Added
    App that navigate between two Activities an return data from one to
    other

---
+++ HelloWorld/Project/HelloWorld/app/src/main/java/com/android/jrios/helloworld/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.android.jrios.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onLogin(View v)
+    {
+        TextView tv = (TextView) findViewById(R.id.editTextUsuario);
+        Intent i = new Intent();
+        i.putExtra(MainActivity.USER_NAME,tv.getText().toString());
+        setResult(RESULT_OK, i);
+        finish();
+    }
+}
---
+++ HelloWorld/Project/HelloWorld/app/src/main/java/com/android/jrios/helloworld/MainActivity.java
@@ -0,0 +1,78 @@
+package com.android.jrios.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String USER_NAME;
+    public static int LOGIN_OK = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onNext(View v)
+    {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivityForResult(i,LOGIN_OK);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == LOGIN_OK)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                String user = data.getExtras().getString(USER_NAME);
+                Toast t = Toast.makeText(this, "Bienvenido " + user, Toast.LENGTH_LONG);
+                t.show();
+            }
+        }
+    }
+}
